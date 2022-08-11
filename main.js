bf87487760fd0914f206caa47ad2b14ac3d47e2f
vansh <vansh0932.be20@chitkara.edu.in>
2022-08-11 14:33:10 +0530

    cmplt

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-const xhr = new XMLHttpRequest()
+document.querySelector('#fetchbtn').addEventListener('click',()=>{
+    const xhr= new XMLHttpRequest()
+    xhr.open('GET','https://jsonplaceholder.typicode.com/photos',true);
+    xhr.onload=function(){
+        const response = JSON.parse(xhr.responseText);
+
+        let temp="";
+        for(let i=0;i<response.length;i++){
+            let id=response[i].thumbnailUrl;
+            let title =response[i].title;
+            temp+=`
+            <div style="text-align">
+            <img src="${id}" style=" margin-right=50px;">
+            <div style="width:120px">
+            <p style="overflow:auto">${title}</p>
+            </div>
+        </div>
+            `
+        }
+        document.querySelector('#images').innerHTML=temp
+    }
+    xhr.send()
+})
